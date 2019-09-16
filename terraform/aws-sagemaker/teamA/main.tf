79511440a1963b06bd6663e9285b4602a6c00a6c
Rutwik Bharadwaj <Rutwik.Bharadwaj@nike.com>
2019-09-15 20:40:12 -0700

    commit test

--- terraform/aws-sagemaker/teamA/main.tf
+++ terraform/aws-sagemaker/teamA/main.tf
@@ -4,8 +4,8 @@ provider "aws" {
   secret_key = "${var.secret_key}"
 }
 
-module "dynamo_table" { 
-    source = "https://github.com/eduever/devops.git//devops/terraform/aws-sagemaker/sagemaker/?ref=master"
+module "sagemaker" { 
+    source = "github.com/eduever/devops.git//devops/terraform/aws-sagemaker/sagemaker?ref=master"
     sg_notebook_name = "teamA-sg"
     sg_notebook_instance = "ml.t2.medium"
     sg_notebook_role = "arn:aws:iam::175546642044:role/service-role/AmazonSageMaker-ExecutionRole-20190915T161542"
